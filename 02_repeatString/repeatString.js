e19c21b00cd3811d8a418d6e8e6e21bf4de7120b
4rn4u <vilx00@protonmail.com>
2024-06-02 22:28:34 +0200

    ex 1 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let ret= '';
+    if (times < 0){
+        return "ERROR"
+    }
+    for(let i = 0; i<times; i++){
+        ret += word;
+    }
+    return ret;
 };
 
 // Do not edit below this line
